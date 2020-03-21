f9a4216fff70ec58666fc2499359254f9431b0bd
alice <bow1226@gmail.com>
2020-03-21 14:42:48 +0800

    feat: Implement method send

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include "TrafficLight.h"
+#include <queue>
 
 /* Implementation of class "MessageQueue" */
 
@@ -17,6 +18,9 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex>
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> uLock(_mutex);
+    _queue.push_back(std::move(msg));
+    _condition.notify_one();
 }
 
 /* Implementation of class "TrafficLight" */
